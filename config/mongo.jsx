bd14f2f47f2afc811b6a7feeb5e46ec0f0798f8f
Foxy20122012 <enriquemerlos49@gmail.com>
2023-02-05 23:10:55 -0600

    node

---
+++ config/mongo.jsx
@@ -0,0 +1,22 @@
+const mongoose = require ("mongoose");
+
+const dbConnect = ()=>{
+    const DB_URI = process.env.DB_URI;
+    mongoose.connect(
+        DB_URI, 
+        {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+},
+(err,res)=>{
+    if(!err){
+        console.log('****Conexion Establecida***')
+    }else{
+        console.log('***Conexion Erronea***')
+    }
+}
+);
+
+};
+
+module.exports=dbConnect;
